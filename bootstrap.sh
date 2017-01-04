6158a49c71fcd4d59766b6f20656c491e529fd83
Mark Winterbottom <mark@londonappdeveloper.com>
2017-01-04 10:50:33 +0000

    Updated the bootstrap script to install MySQL.

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,4 +4,10 @@
 sudo apt-get update
 
 # Install required packages.
-sudo apt-get install python3 python python-pip python-dev libmysqlclient-dev mysql-server
+sudo apt-get install -y language-pack-en
+sudo apt-get install -y python3 python python-pip python-dev
+
+# Install MySQL Server
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password rootpass'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password rootpass'
+sudo apt-get install -y libmysqlclient-dev mysql-server
