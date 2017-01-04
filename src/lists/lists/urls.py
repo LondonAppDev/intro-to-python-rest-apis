a1c51789c2101996fa36fde83b61983d587c1c77
Mark Winterbottom <mark@londonappdeveloper.com>
2017-01-04 20:34:23 +0000

    Added project and app.

--- src/lists/lists/urls.py
+++ src/lists/lists/urls.py
@@ -1,4 +1,4 @@
-"""list_app URL Configuration
+"""lists URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
--- src/lists/lists/wsgi.py
+++ src/lists/lists/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for list_app project.
+WSGI config for lists project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "list_app.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lists.settings")
 
 application = get_wsgi_application()
