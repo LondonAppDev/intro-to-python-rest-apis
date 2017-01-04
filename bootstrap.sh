a69e941086e7bf16276d3b5afc48b8b9ed25de9e
Mark Winterbottom <mark@londonappdeveloper.com>
2017-01-04 10:11:18 +0000

    Updated gitignore to exclude .log files. Updated bootstrap script to install mysql-server.

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,5 +3,5 @@
 # Update the apt repos.
 sudo apt-get update
 
-# Install required packages
-sudo apt-get install python3 python python-pip python-dev libmysqlclient-dev
+# Install required packages.
+sudo apt-get install python3 python python-pip python-dev libmysqlclient-dev mysql-server
