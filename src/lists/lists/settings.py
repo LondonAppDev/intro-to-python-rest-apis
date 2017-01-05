feba8d8b0171478515bfcf70c8c3b58d4dc56a39
Mark Winterbottom <mark@londonappdeveloper.com>
2017-01-05 10:39:19 +0000

    Added the database settings in the settings.py file.

--- src/lists/lists/settings.py
+++ src/lists/lists/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'lists.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'lists_db',
+        'USER': 'root',
+        'PASSWORD': 'rootpass',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
