ddc4dd253900bf9beef3699224ea948162af115d
Mark Winterbottom <mark@londonappdeveloper.com>
2017-01-05 15:39:39 +0000

    Added the list item viewset to the views.py file.

--- src/lists/lists_api/views.py
+++ src/lists/lists_api/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from models import ListItem
+from rest_framework import serializers, viewsets
 
-# Create your views here.
+
+class ListItemSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ListItem
+        fields = ('name',)
+
+
+class ListItemViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for managing list items.
+    """
+
+    serializer_class = ListItemSerializer
+    queryset = ListItem.objects.all()
